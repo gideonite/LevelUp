216e90f10679a0152742acf4c76aecef6ea63bab
Gideon Dresdner <gideond@gmail.com>
2012-08-31 13:26:17 -0400

    loop cbs on multiple probe files

--- signal_chr_pos.py
+++ signal_chr_pos.py
@@ -2,14 +2,18 @@
 
 """ Usage:
 signal_chr_pos_toCBS.py   --m=marker_file ...
-                    --p=probe_file
+                    --p=probe_file ...
                     [--O=output_file ]
-                    [--sample_name=<sample_name>]
 
 Options:
   --O=output_file   [default: cbs.out]
 
 """
+
+# todo: should I give users the choice to name the sample?
+# [--sample_name=<sample_name>]
+
+# todo: improve the name of this output file
 
 from docopt import docopt
 import os
@@ -80,10 +84,13 @@ if __name__ == '__main__':
 
     args = docopt(__doc__)
 
+    #print args
+    #boom
+
     #marker_files = ['test_data/marker.na30.lst', 'test_data/marker.na31.lst']
     marker_files = args['--m']
     #probe_f = 'test_data/SN6.CN.test.data.txt'
-    probe_f = args['--p']
+    probe_files = args['--p']
 
     SCP_OUT = 'signal_chr_pos.out.tmp'
     ## todo : this is bad.  factor it out
@@ -92,25 +99,31 @@ if __name__ == '__main__':
     hash = marker_position_hash(marker_files)
     print "done!"
 
-    print "...mapping probes to chromosome positions..."
-    s_c_p = signal_chr_pos(probe_f, hash)
-    print "done!"
 
-    # write this to a file to pass to R
-    s_c_p_out = open(SCP_OUT, 'w')
-    s_c_p_out.write('signal\tchr\tpos\n');    # write the column names (a.k.a header)
-    for row in s_c_p:
-        s_c_p_out.write("%s\n" % '\t'.join(row))
-        # write the row to the file
-        # tab-deliminited
-        # rows separated by new line
+    # -- CBS -- #
+    for probe_f in probe_files:
 
-    s_c_p_out.close()
+        print "...mapping probes to chromosome positions for <" + probe_f + ">" + " ..."
+        s_c_p = signal_chr_pos(probe_f, hash)
+        print "done!"
 
-    # run cbs
-    cbs_cmd = [SCP_OUT, args['--O'], args['--sample_name']]      # args
-    cbs_cmd = 'Rscript cbs.r ' + ' '.join(cbs_cmd)
-    os.system(cbs_cmd)
+        # write this to a file to pass to R
+        s_c_p_out = open(SCP_OUT, 'w')
+        s_c_p_out.write('signal\tchr\tpos\n');    # write the column names (a.k.a header)
+        for row in s_c_p:
+            s_c_p_out.write("%s\n" % '\t'.join(row))
+            # write the row to the file
+            # tab-deliminited
+            # rows separated by new line
 
-    # debug : don't remove this file
-    os.system('rm ' + SCP_OUT)
+        s_c_p_out.close()
+
+        sample_name = os.path.basename(probe_f)
+
+        # run cbs
+        cbs_cmd = [SCP_OUT, args['--O'], sample_name]      # args
+        cbs_cmd = 'Rscript cbs.r ' + ' '.join(cbs_cmd)
+        os.system(cbs_cmd)
+
+        # debug : don't remove this file
+        os.system('rm ' + SCP_OUT)
